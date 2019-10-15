22caaec4dbafc96abc7b6352646b2803a7fa47f4
Mr. Wu <wushunyan@163.com>
2019-10-15 22:37:37 +0800

    update

--- setting.py
+++ setting.py
@@ -4,5 +4,6 @@ stopwords_path = os.path.join(os.path.abspath('./'), 'data', 'stop_words.txt')
 word2vec_path = os.path.join(os.path.abspath('./'), 'data', 'word2vec.model')
 news_path = os.path.join(os.path.abspath('./'), 'data', 'sqlResult_1558435.csv')
 words_frequence_path = os.path.join(os.path.abspath('./'), 'data', 'words_frequence.pk')
+corpus_path = os.path.join(os.path.abspath('./'), 'data', 'corpus.txt')
 
 summary_ratio = 6
